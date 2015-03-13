6c1837c508ba9a6e9b3ad15e6a7de26d90ade4b2
root <root@iZ25p15bj1rZ.(none)>
2015-03-13 14:46:58 +0800

    baseline of weixin dev

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('polls.views',
     url(r'^(?P<poll_id>\d+)/$', 'detail'),
     url(r'^(?P<poll_id>\d+)/results/$', 'results'),
     url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+
+    url(r'^wx_main$', 'wx_main'),
+
 )
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from polls.models import Poll, Choice
 import json
+import hashlib, time, re
+from xml.etree import ElementTree as ET
+from django.utils.encoding import smart_str
+from django.views.decorators.csrf import csrf_exempt
+
 
 def jsonp(request):
     return HttpResponse(json.dumps([1,2,3,4,5, None, True]), content_type="application/json")
+
+
+WEIXIN_TOKEN = "JACKY"
+REPLY_TMPL = """<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[%s]]></Content>
+                <FuncFlag>0</FuncFlag>
+            </xml>"""
+
+@csrf_exempt
+def wx_main(request):
+    """
+    所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+    微信接入验证是GET方法，
+    微信正常的收发消息是用POST方法。
+    """
+    print request.method, request.path, request.GET, request.POST
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = WEIXIN_TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("weixin main (signature wrong)")
+    else:   ### POST
+        xml_str = smart_str(request.body)
+        print xml_str
+        xml = ET.fromstring(xml_str)
+        content = xml.find("Content").text
+        fromUserName = xml.find("ToUserName").text
+        toUserName = xml.find("FromUserName").text
+        postTime = str(int(time.time()))
+        if not content:
+            return HttpResponse(REPLY_TMPL % (toUserName, fromUserName, postTime, "输入点命令吧...http://chajidian.sinaapp.com/ "))
+        if content == "Hello2BizUser":
+            return HttpResponse(REPLY_TMPL % (toUserName, fromUserName, postTime, "查询成绩绩点请到http://chajidian.sinaapp.com/ 本微信更多功能开发中..."))
+        else:
+            return HttpResponse(REPLY_TMPL % (toUserName, fromUserName, postTime, "暂不支持任何命令交互哦,功能开发中..."))
+        response_xml = WEIXIN_REPLY_TMPL
+        return HttpResponse(response_xml)
+
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
